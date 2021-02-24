6221e0ac112d06f4a6c3f23c211c31153aa6bc8a
Jason Chu <jason.chu614@gmail.com>
2021-02-24 11:07:18 -0800

    initial commit, finished project

---
+++ data.js
@@ -0,0 +1,88 @@
+import bcrypt from 'bcrypt';
+
+const data = {
+    users: [
+        {
+            name: "Jason",
+            email:"admin@admin.com",
+            password:bcrypt.hashSync('rustyrusty', 8),
+            isAdmin: true,
+        },
+        {
+            name: "Jon",
+            email:"dude@dude.com",
+            password:bcrypt.hashSync('123123', 8),
+            isAdmin: false,
+        },
+    ],
+    products: [
+        {
+            name: 'Nike Slim Shirt',
+            category: 'Shirts',
+            images: '/images/product-1.jpg',
+            price: 120,
+            countInStock: 12,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Adidas Slim Shirt',
+            category: 'Shirts',
+            images: '/images/product-2.jpg',
+            price: 100,
+            countInStock: 5,
+            brand: 'Adidas',
+            rating: 4,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Lacoste Slim Shirt',
+            category: 'Shirts',
+            images: '/images/product-3.jpg',
+            price: 220,
+            countInStock: 15,
+            brand: 'Lacoste',
+            rating: 4.8,
+            numReviews: 17,
+            description: 'high quality product',
+        },
+        {
+            name: 'Nike Slim Pants',
+            category: 'Pants',
+            images: '/images/product-4.jpg',
+            price: 137,
+            countInStock: 10,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 14,
+            description: 'high quality product',
+        },
+        {
+            name: 'Puma Slim Pants',
+            category: 'Pants',
+            images: '/images/product-5.jpg',
+            price: 60,
+            countInStock: 20,
+            brand: 'Puma',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+        {
+            name: 'Adidas Slim Pants',
+            category: 'Pants',
+            images: '/images/product-6.jpg',
+            price: 139,
+            countInStock: 0,
+            brand: 'Adidas',
+            rating: 4.5,
+            numReviews: 10,
+            description: 'high quality product',
+        },
+    ]
+}
+
+export default data;
