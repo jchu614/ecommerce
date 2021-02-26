f1da63630ec4958ef1b298b1df6117ed7f5606ef
Jason Chu <jason.chu614@gmail.com>
2021-02-26 08:06:59 -0800

    heroku deploy

--- models/productModel.js
+++ models/productModel.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 const reviewSchema = new mongoose.Schema(
     {
-        name: {type: String, required: true, unique: true},
+        name: {type: String, required: true, unique: false},
         comment: {type: String, required: true},
         rating: {type: Number, required: true}
     },
